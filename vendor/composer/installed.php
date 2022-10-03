08ca065d8360a4d07fd90b09ffe27018e57a8790
ShiroKatake <tgkhang262@gmail.com>
2022-10-03 23:58:26 +1100

    fix: removed .gitignore (wasn't needed)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e851d73ecc5251b529316e2346312e7127996881',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e851d73ecc5251b529316e2346312e7127996881',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'alrik11es/cowsayphp' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => '080dc10c8f5420fef35163569293601f17124eeb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../alrik11es/cowsayphp',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v224',
+            'version' => '224.0.0.0',
+            'reference' => '2df7a440119b0c9468fbace658eb0d528b4d6971',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
