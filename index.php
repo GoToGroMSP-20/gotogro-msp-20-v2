283d1a46f45b60e057488639ef81c74f506da334
ShiroKatake <tgkhang262@gmail.com>
2022-10-04 18:48:35 +1100

    feat(template): created a pointer to the default file on first load

--- index.php
+++ index.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <link href="./styles/styles.css" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="/scripts/navbar.js"></script>
-  </head>
-  <body>
-    <?php include_once ("nav.inc"); ?>
-    <main>
-      This is a page :DDDD
-    </main>
-  </body>
-</html>
+<?php header("Location: template.php"); ?>
+<!-- This will need to be addOrder.php once the file is created -->
---
+++ template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link href="./styles/styles.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="/scripts/navbar.js"></script>
+  </head>
+  <body>
+    <?php include_once ("navbar.inc"); ?>
+    <main>
+      This is a page :DDDD
+    </main>
+  </body>
+</html>
