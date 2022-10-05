3c74136de7f74662cefc039858c4e16030f27376
Baavi <87794520+Baavi@users.noreply.github.com>
2022-10-06 01:21:22 +1100

    Minor UI Changes

--- scripts/addorder.js
+++ scripts/addorder.js
@@ -1,24 +1,25 @@
-
 function show_row2() {
-    var r1_price = document.getElementById("price1").value.trim();
-    var r2 = document.getElementById("row_2")
-    if (!r1_price == ""){
-      r2.style.display = "block";
-    }else{
-      r2.style.display = "none";
-    }
+  var r1_price = document.getElementById("price1").value.trim();
+  var r2 = document.getElementById("row_2");
+  if (r1_price != "") {
+    r2.style.display = "flex";
+  } else {
+    r2.style.display = "none";
+  }
 }
 
-  function show_total() {
-    var r1_price = document.getElementById("price1").value.trim();
-    var r1_qty = document.getElementById("quantity1").value.trim();
-    var r2_price = document.getElementById("price2").value.trim();
-    var r2_qty = document.getElementById("quantity2").value.trim();
-    document.getElementById("total_price").innerText =  "Total Price : 0";
-    if ((!r1_price == "") && (!r1_qty == "")){
-        document.getElementById("total_price").innerText = "Total Price : " + r1_price * r1_qty;
-    }
-    if ((!r1_price == "") && (!r1_qty == "")&&(!r2_price == "") && (!r2_qty == "")){
-        document.getElementById("total_price").innerText = "Total Price : " +  ((r1_price * r1_qty) + (r2_price * r2_qty));
-    }
+function show_total() {
+  var r1_price = document.getElementById("price1").value.trim();
+  var r1_qty = document.getElementById("quantity1").value.trim();
+  var r2_price = document.getElementById("price2").value.trim();
+  var r2_qty = document.getElementById("quantity2").value.trim();
+  document.getElementById("total_price").innerText = "Total Price : $0";
+  if (!r1_price == "" && !r1_qty == "") {
+    document.getElementById("total_price").innerText =
+      "Total Price : $" + r1_price * r1_qty;
   }
+  if (!r1_price == "" && !r1_qty == "" && !r2_price == "" && !r2_qty == "") {
+    document.getElementById("total_price").innerText =
+      "Total Price : $" + (r1_price * r1_qty + r2_price * r2_qty);
+  }
+}
---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+$host = "eu-cdbr-west-03.cleardb.net";
+$user = "beb99f0a7ad238";
+$pwd = "19cb8c1d";
+$sql_db = "heroku_b46d5af56520402";
+?>
