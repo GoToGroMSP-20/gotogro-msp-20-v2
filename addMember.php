f03f056319dfb90e56b2376a4d67fb2545411bf6
kravocados <krati.awasthi.nz@gmail.com>
2022-10-07 00:33:16 +1100

    fix)06-Add-Member-Form): indent and styling fix

--- addMember.php
+++ addMember.php
@@ -1,33 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link href="./styles/styles.css" rel="stylesheet" type="text/css"/>
-  </head>
-  <body>
-    <?php include_once ("navbar.inc"); ?>
-    <main>
-    <form method="post" action="processMEMBER.php" novalidate="novalidate">
-      <h1>Add New Member</h1>
+
+<head>
+  <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <?php include_once("navbar.inc"); ?>
+  <main>
+    <form method="post" action="processMEMBER.php" novalidate="novalidate" class="form">
+      <div class="form-input">
+        <h1>Add New Member</h1>
         <h3>Personal Info</h3>
-    <br>
-      <label for="fname">First name:</label><br>
-      <input type="text" id="fname" name="fname" required>
-    <br>
-      <label for="lname">Last name:</label><br>
-      <input type="text" id="lname" name="lname"  required>
-    <br>
-    <label for="date">Date of Birth:</label><br>
-    <input type="date" id="date" name="date" required>
-    <br>
-    <label for="email">Email:</label><br>
-    <input type="text" id="email" name="email" required>
-    <br>
-    <label for="mnumber">Mobile Number (Optional):</label><br>
-    <input type="text" id="mnumber" name="mnumber">
-     <br>
-     <button class="button" buttonType="primary" type="submit" name="submit">Add Member</button>
-  </form>
-    </main>
-  </body>
-    <script type="text/javascript" src="/scripts/navbar.js"></script>
+      </div>
+      <div class="form-input">
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname" required>
+      </div>
+      <div class="form-input">
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname" required>
+      </div>
+      <div class="form-input">
+        <label for="date">Date of Birth:</label>
+        <input type="date" id="date" name="date" required>
+      </div>
+      <div class="form-input">
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" required>
+      </div>
+      <div class="form-input">
+        <label for="mnumber">Mobile Number (Optional):</label>
+        <input type="text" id="mnumber" name="mnumber">
+      </div>
+      <div class="button">
+        <button class="button" id=button buttonType="primary" type="submit" name="submit">Add Member</button>
+      </div>
+    </form>
+  </main>
+</body>
+<script type="text/javascript" src="/scripts/navbar.js"></script>
+
 </html>
