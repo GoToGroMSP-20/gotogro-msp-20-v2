29c40b733411a5142a78a56ca53fe694e7a7986e
kravocados <krati.awasthi.nz@gmail.com>
2022-10-07 22:53:01 +1100

    09-go-back-Link

---
+++ backLink.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <?php include_once("navbar.inc"); ?>
+    <main>
+        <div>
+            <a class="back-item" href="/memberDetails.php">
+                <?php echo file_get_contents("./assets/icons/FiArrowLeft.svg"); ?> Go Back
+            </a>
+        </div>
