de9e43064206b309cb9b3d3a523308a2cfd32d9b
Vaissheenavi Prabakaran <vaissheenavi.prabakaran@gmail.com>
2022-10-07 22:59:42 +1100

    Changes done accordingly

--- editMember.php
+++ editMember.php
@@ -1,68 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="description" content="Edit Member Form" />
-    <meta name="keywords" content="HTML, CSS, JavaScript" />
-    <meta name="author" content="MSP Group 20" />
-    
-    <link href="styles/editMember.css" rel="stylesheet" type="text/css" />
 
-    <title>Edit Member Form</title>
+<head>
+  <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+</head>
 
-    
-    
-  </head>
-  <body>
+<body>
+<?php include_once("navbar.inc"); ?>
 
+  <main>
+    <form method="post" action="processMEMBER.php" novalidate="novalidate" class="editMemberform">
 
-<h2>Edit Member Form</h2>
+      <div class="editFormInput">
+        <h3>Edit Member Form</h3>
+      </div>
 
-<form id="editMemberform"> 
+      <div class="editFormInput">
+        <label for="firstname" id="fName">First Name</label>
+        <input type="text" pattern="[a-zA-Z]{2,20}" id="firstname" name="firstname" required />
+      </div>
 
-    
-        <!-- <legend style="font-size:19px"><strong>Member Details</strong></legend> -->
-   <p>
-      <label for="firstname" id="fName" style="font-size:19px">First Name</label>
-      <input type="text" pattern="[a-zA-Z]{2,20}" id="firstname" name="firstname" required/>
-   
-      <label for="lastname" id="lName" style="font-size:19px">Last Name</label>
-      <input type="text" pattern="[a-zA-Z]{2,20}" id="lastname" name="lastname" required/> 
-    </p>
-    <p> 
-      <label for ="DateofBirth" id="dob" style="font-size:19px">Date of Birth</label>
-      <input type="date" pattern="\d{1,2}\/\d{1,2}\/\d{4}" name="dateofbirth" id="DateofBirth" required/>
-      
-   </p>
+      <div class="editFormInput">
+        <label for="lastname" id="lName">Last Name</label>
+        <input type="text" pattern="[a-zA-Z]{2,20}" id="lastname" name="lastname" required />
+      </div>
 
-   <p> 
-    <label for="email" id="em" style="font-size:19px">Email</label>
-    <input type="email" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
-    </p>
-    
-    <p>
-    <label for="phonenum" id="mobilenum" style="font-size:19px">Mobile Number (Optional)</label>
-    <input type="tel" name= "phonenum" id="phonenum" maxlength="12" pattern="[\d\s]{8,12}" placeholder="For eg. 0400 000 000" required/>
-    </p>
+      <div class="editFormInput">
+        <label for="DateofBirth" id="dob">Date of Birth</label>
+        <input type="date" pattern="\d{1,2}\/\d{1,2}\/\d{4}" name="dateofbirth" id="dateofbirth" required />
+      </div>
 
- 
+      <div class="editFormInput">
+        <label for="email" id="em">Email</label>
+        <input type="email" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required />
+      </div>
 
-
-<a id=cancelButton>
-    <input type="reset" id="cancel" value="Cancel"/>
-</a>
-
-<button id="button">
-    <input type="submit" id= "sub" value="Submit"/>
-</button>
-
-
-</form>
+      <div class="editFormInput">
+        <label for="phonenum" id="mobilenum">Mobile Number (Optional)</label>
+        <input type="tel" name="phonenum" id="phonenum" maxlength="12" pattern="[\d\s]{8,12}" placeholder="For eg. 0400 000 000" required />
+      </div>
 
 
 
 
+      <div class="button">
+        <button class="button" id=button buttonType="primary" type="submit" name="submit">Save Details</button>
+      </div>
+
+      <div class="buttonCancel">
+        <button class="buttonCancel" id=button buttonType="secondary" type="reset" name="cancel">Cancel</button>
+      </div>
 
 
-</body>  
+    </form>
+</main>
+</body>
+<script type="text/javascript" src="/scripts/navbar.js"></script>
+
 </html>
