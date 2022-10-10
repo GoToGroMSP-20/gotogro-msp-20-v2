fb7b82660c18ef7a17d037222461f174916b1624
Baavi <87794520+Baavi@users.noreply.github.com>
2022-10-11 04:23:55 +1100

    Add Order Bug Fix

--- processORDER.php
+++ processORDER.php
@@ -13,8 +13,8 @@ if (isset($_POST["submit"])) {
         $member_id = $_POST["member_id"];
         $member_id = sanitise_input($member_id);
         //echo "<p> Member ID : $member_id</p>";
-        if ((!preg_match("/^[\d]{10}$/", $member_id)) && (!filter_var($member_id, FILTER_VALIDATE_EMAIL))) {
-            header("location: index.php?member_id=invalid");
+        if (!filter_var($member_id, FILTER_VALIDATE_EMAIL)) {
+            header("location: index.php?order=invalid_id");
             exit();
         }
         $product_ID1 = sanitise_input($_POST["product_ID1"]);
@@ -28,27 +28,39 @@ if (isset($_POST["submit"])) {
         if (!$conn) {
             // Display error msg
             echo "<p>Database connection failure </p>";
-            header("location: index.php?db=failure");
+            header("location: index.php?order=connection_failure");
             exit();
         } else {
-            $query = "INSERT INTO MemberOrder (productName, quantity, unitPrice, memberId, datePurchased ) VALUES('$product_ID1','$quantity1','$price1','$member_id','$date');";
-
-            $insert_result = mysqli_query($conn, $query);
-            $last_id = $conn->insert_id;
-
+            $user_query = "SELECT * FROM member WHERE email = '$member_id'";
+            $user_result = mysqli_query($conn, $user_query);
+            $row = mysqli_fetch_assoc($user_result);
+            $memberId = $row["memberId"];
+            echo "<p> DB member ID: $memberId</p>";
             // checks if the execution was successful
-            if (!$insert_result) {
-                echo "<p>Something is wrong with ", $query, "</p>";
-                header("location: index.php?db=invalid_query");
+            if (!$row) {
+                echo "<p class='manage_error'>Something is wrong with ", $query, "</p>";
+                header("location: index.php?Username=invalid_member_id");
                 exit();
+            } else {
+                $query = "INSERT INTO MemberOrder (productName, quantity, unitPrice, memberId, datePurchased ) VALUES('$product_ID1','$quantity1','$price1','$memberId','$date');";
+
+                $insert_result = mysqli_query($conn, $query);
+                $last_id = $conn->insert_id;
+
+                // checks if the execution was successful
+                if (!$insert_result) {
+                    echo "<p>Something is wrong with ", $query, "</p>";
+                    header("location: index.php?order=invalid_query");
+                    exit();
+                }
             }
             // close the database connection
             mysqli_close($conn);
         }
-        header("location: index.php?form=$last_id");
+        header("location: index.php?order=success");
         exit();
     } else {
-        header("location: index.php?member_id=empty");
+        header("location: index.php?order=empty");
         exit();
     }
 } else {
