d6fd23fec1449c9aa02cc6ddcfe857d0ce7bac49
kravocados <krati.awasthi.nz@gmail.com>
2022-10-11 20:53:57 +1100

    feat(DEV) 12-Add-Member-Success/Failure-Dialog-box

---
+++ addMemberAlert.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <?php include_once("navbar.inc"); ?>
+  <main>
+    <!-- Success dialog -->
+    <dialog class="success">
+      <div class="popup-status">
+        <?php echo file_get_contents("./assets/icons/FaCheckCircle.svg"); ?>
+        <p>Added Membership Successful</p>
+      <button class="button" onclick="location.href = '/addOrder.php';" buttonType="primary" type="submit" name="submit">Add an Order</button>
+      </div>
+    </dialog>
+
+    <!-- Error dialog -->
+    <dialog class="error">
+      <div class="popup-status">
+        <?php echo file_get_contents("./assets/icons/FaTimesCircle.svg"); ?>
+        <p>Unable to Add Member. Something broke from our end. Please try again (Error code: 500)</p>
+      </div>
+      <button class="button" onclick="location.href = '/addMember.php';" buttonType="primary" type="submit" name="submit">Back to Add Member</button>
+    </dialog>
+
+  </main>
+</body>
+<script type="text/javascript" src="/scripts/navbar.js"></script>
+<script type="text/javascript" src="/scripts/dialog.js"></script>
+
+</html>
