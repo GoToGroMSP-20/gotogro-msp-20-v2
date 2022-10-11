44517e2a225c133cca373055dda85d9e081876c3
Baavi <87794520+Baavi@users.noreply.github.com>
2022-10-12 03:36:27 +1100

    Add Member Form Validation

--- addMember.php
+++ addMember.php
@@ -8,33 +8,34 @@
 <body>
   <?php include_once("navbar.inc"); ?>
   <main>
-    <form method="post" action="processMEMBER.php" novalidate="novalidate" class="form">
+    <form method="post" action="processMEMBER.php" class="form">
       <div class="form-input">
         <h1>Add New Member</h1>
         <h3>Personal Info</h3>
       </div>
       <div class="form-input">
         <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname" required>
+        <input type="text" pattern="[a-zA-Z]{2,20}" id="fname" name="fname" required>
       </div>
       <div class="form-input">
         <label for="lname">Last name:</label>
-        <input type="text" id="lname" name="lname" required>
+        <input type="text" pattern="[a-zA-Z]{2,20}" id="lname" name="lname" required>
       </div>
       <div class="form-input">
         <label for="date">Date of Birth:</label>
-        <input type="date" id="date" name="date" required>
+        <input type="date" pattern="^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$" id="date" name="date" required>
       </div>
       <div class="form-input">
         <label for="email">Email:</label>
-        <input type="text" id="email" name="email" required>
+        <input type="text" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
       </div>
       <div class="form-input">
         <label for="mnumber">Mobile Number (Optional):</label>
-        <input type="text" id="mnumber" name="mnumber">
+        <input type="text" id="mnumber" name="mnumber" maxlength="10" pattern="[\d]{10}" placeholder="For eg. 0400000000">
       </div>
       <div class="button addMember">
-        <button class="button addMember" id=button buttonType="primary" type="submit" name="submit">Add Member</button>
+        <button class="button addMember" id=button buttonType="primary" type="submit" name="submit">Add
+          Member</button>
       </div>
     </form>
   </main>
