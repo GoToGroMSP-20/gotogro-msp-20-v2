d404c492da160cbef2805bfefa33c2a8c535ecf2
Rory Howlett <103076693@student.swin.edu.au>
2022-10-13 14:10:59 +1100

    feat(fix, testing): changed the "get order" query to work with new db. added a way to test overflow scrolling.

--- salesReport.php
+++ salesReport.php
@@ -17,7 +17,7 @@
         <div class="popup-status">
             <?php echo file_get_contents("./assets/icons/FaTimesCircle.svg"); ?>
             <p>Oops! Something broke from our end. Please contact our technicians for support (Error code: 500)</p>
-            <button class="button" onclick="location.href = '/addOrder.php';" buttonType="primary" type="submit" name="submit">Back to Add Order</button>
+            <button class="button" onclick="location.href = '/index.php';" buttonType="primary" type="submit" name="submit">Back to Add Order</button>
         </div>
     </dialog>
 
@@ -25,7 +25,7 @@
     <?php include_once("navbar.inc"); ?>
     <main>
         <div class="salesReport">
-            <h2>Sales Report</h2>
+            <h1>Sales Report</h1>
             <?php
                 require_once("settings.php"); 
                 $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
@@ -33,12 +33,59 @@
                     echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
                     exit();
                 } else {
-                    $sql_table = "MemberOrder";
-                    $user_query =  "SELECT productName, SUM(quantity) AS totalQuantity, unitPrice, SUM(quantity * unitPrice) AS totalPrice, datePurchased 
-                                    FROM $sql_table 
-                                    WHERE datePurchased > NOW() - INTERVAL 30 day 
-                                    GROUP BY productName
-                                    ORDER BY productName";
+
+
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+                    ////////////////                     Actual Thing                               //////////////
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+                    // $user_query =  "SELECT o.product_id, p.name, SUM(o.quantity) AS totalQuantity, p.price, (SUM(o.quantity) * p.price) AS totalPrice, t.date_purchased 
+                    //                 FROM product p
+                    //                 INNER JOIN transactionorder o
+                    //                 ON p.product_id = o.product_id
+                    //                 INNER JOIN transaction t 
+                    //                 ON o.transaction_id = t.transaction_id
+                    //                 WHERE t.date_purchased > NOW() - INTERVAL 30 day 
+                    //                 GROUP BY o.product_id
+                    //                 ORDER BY o.product_id";
+                    // $user_result = mysqli_query($conn, $user_query);       
+                    // if(!$user_result) {
+                    //     echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
+                    //     exit();
+                    // } else if(mysqli_num_rows($user_result) == 0) {
+                    //     echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
+                    //     exit();
+                    // } else {
+                    //     echo             
+                    //     "<table>
+                    //         <thead>
+                    //             <tr>
+                    //                 <th>Item ID</th>
+                    //                 <th>Product Name</th>
+                    //                 <!-- <th>Category</th> -->
+                    //                 <th>QTY</th>
+                    //                 <th>Unit Price</th>
+                    //                 <th>Price Total</th>
+                    //             </tr>
+                    //         </thead>           
+                    //         <tbody>";
+                    //     while ( $row = mysqli_fetch_assoc($user_result) ){
+                    //         echo "<tr>
+                    //                 <td>{$row['product_id']}</td>
+                    //                 <td>{$row['name']}</td>
+                    //                 <td>{$row['totalQuantity']}</td>
+                    //                 <td>{$row['price']}</td>
+                    //                 <td>{$row['totalPrice']}</td>
+                    //                 </tr>\n";
+                    //     }
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+                    ////////////////                     Actual Thing                               //////////////
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+                    ////////////////          To Test Scrolling Functionality of Table              //////////////
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+                    $user_query =  "SELECT member_id, lastName, firstName, dob, email FROM Member";
                     $user_result = mysqli_query($conn, $user_query);       
                     if(!$user_result) {
                         echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
@@ -51,23 +98,28 @@
                         "<table>
                             <thead>
                                 <tr>
-                                    <!-- <th>Item ID</th> -->
-                                    <th>Product Name</th>
-                                    <!-- <th>Category</th> -->
-                                    <th>QTY</th>
-                                    <th>Unit Price</th>
-                                    <th>Price Total</th>
+                                    <th>Member_ID</th>
+                                    <th>Last Name</th>
+                                    <th>First Name</th>
+                                    <th>DOB</th>
+                                    <th>Email</th>
                                 </tr>
                             </thead>           
                             <tbody>";
                         while ( $row = mysqli_fetch_assoc($user_result) ){
                             echo "<tr>
-                                    <td>{$row['productName']}</td>
-                                    <td>{$row['totalQuantity']}</td>
-                                    <td>{$row['unitPrice']}</td>
-                                    <td>{$row['totalPrice']}</td>
+                                    <td>{$row['member_id']}</td>
+                                    <td>{$row['lastName']}</td>
+                                    <td>{$row['firstName']}</td>
+                                    <td>{$row['dob']}</td>
+                                    <td>{$row['email']}</td>
                                     </tr>\n";
                         }
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+                    ////////////////          To Test Scrolling Functionality of Table              //////////////
+                    //////////////////////////////////////////////////////////////////////////////////////////////
+
+
                         echo 
                         "   </tbody>
                         </table>";
@@ -83,6 +135,5 @@
 <script type="text/javascript" src="./scripts/navbar.js"></script>
 
 </html>
-
 
 
