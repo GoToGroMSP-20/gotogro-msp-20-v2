6be46e28d68b9e28c2215483b048bff73ef7d02b
Vaissheenavi Prabakaran <vaissheenavi.prabakaran@gmail.com>
2022-10-15 00:47:48 +1100

    done

--- memberDetails.php
+++ memberDetails.php
@@ -2,19 +2,19 @@
 <html lang="en">
 
 <head>
-  <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
-  <link rel="icon" href="assets/icons/SiOverleaf.svg" type="image/icon" />
-  <title>GoToGro | Display Member Details</title>
+    <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" href="assets/icons/SiOverleaf.svg" type="image/icon" />
+    <title>GoToGro | Display Member Details</title>
 </head>
 
 
 <body>
 
 
-<?php
-require_once("settings.php"); 
-$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
-if(!$conn) {
+    <?php
+    require_once("settings.php");
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+    if (!$conn) {
         echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
         exit();
     } else {
@@ -23,68 +23,66 @@ if(!$conn) {
         //$member_id = "sessionStorage.memberemail";
 
         $user_query =  "SELECT * FROM member WHERE email = '$member_id'";
-                                   
-        $user_result = mysqli_query($conn, $user_query);       
-        if(!$user_result) {
-        echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
-        exit();
-        } else if(mysqli_num_rows($user_result) == 0) {
-        echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
-        exit();
+
+        $user_result = mysqli_query($conn, $user_query);
+        if (!$user_result) {
+            echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
+            exit();
+        } else if (mysqli_num_rows($user_result) == 0) {
+            echo "<script type='text/javascript' src='./scripts/salesreportdialog.js'></script>";
+            exit();
         } else {
-        $row = mysqli_fetch_assoc($user_result);
+            $row = mysqli_fetch_assoc($user_result);
         }
-    }            
-                
-?>
+    }
+
+    ?>
 
 
-<?php
-  include_once("navbar.inc");
-  ?>
+    <?php
+    include_once("navbar.inc");
+    ?>
     <main>
         <form method="get" action="searchMEMBER.php" class="memberDetailsform">
 
-            <div class="memberDetails">
-            <?php echo "<h1>{$row['firstName']} {$row['lastName']}</h1>" ?>
+            <!-- <div class="memberDetails"> -->
+                <?php echo "<h1>{$row['firstName']} {$row['lastName']}</h1>" ?>
                 <h3>Member Details</h3>
-            </div>
+            <!-- </div> -->
+<div class="memberDetails">
 
 
-            <div class="memberDetails">
-              <b><label for="memberID" id="memberid">Member ID</label></b>
-                
-                <?php
-                    echo "<p>{$row['member_id']}</p>"
-                ?>
+                <div class="inputField">
+                    <b><label for="memberID" id="memberid">Member ID</label></b>
 
-            </div>
+                    <?php echo "<p>{$row['member_id']}</p>" ?>
+                </div>
 
-            <div class="memberDetails" id="MDDOB">
+
+                <div class="inputField2">
                 <b><label for="DateofBirth" id="dob">Date of Birth</label></b>
-                
-                <?php
-                    echo "<p>{$row['dob']}</p>"
-                ?>    
+                <?php echo "<p>{$row['dob']}</p>" ?>
             </div>
 
-            <div class="memberDetails">
-               <b><label for="email" id="em">Email</label></b>
-                
-                <?php
-                    echo "<p>{$row['email']}</p>"
-                ?>   
             </div>
 
+
             <div class="memberDetails">
+
+            <div class="inputField">
+                <b><label for="email" id="em">Email</label></b>
+                <?php echo "<p>{$row['email']}</p>" ?>
+                </div>
+
+            <div class="inputField1">
                 <b><label for="phonenum" id="mobilenum">Mobile Number</label></b>
-                
-                    <?php
-                    echo "<p>{$row['mobile']}</p>"
-                ?>   
+                <?php echo "<p>{$row['mobile']}</p>" ?>
 
                 <input type="hidden" name="member_id" id="member_id" value="1" />
             </div>
+            </div>
+
+
             <div class="button1">
                 <button class="button" id=button1 buttonType="primary" type="submit" name="submit">Edit Details</button>
             </div>
