6e0ea40fe10c81e18a0e3f8c81379eb85db866ad
Baavi <87794520+Baavi@users.noreply.github.com>
2022-10-15 20:25:59 +1100

    37 - More Fields More Dough

--- addOrder.php
+++ addOrder.php
@@ -14,110 +14,175 @@
 <body>
     <?php include_once("navbar.inc"); ?>
     <main>
-    <div class="addorder" id="addorder">
-        <h1 class="white-text">Add New Order</h1>
-        <form method="post" action="processORDER.php" id="form" id="apply" novalidate="novalidate">
-            <div id="row_1">
-                <div class="inputField">
-                    <label for="product_ID1">Product Name</label>
-                    <select name="product_ID1" id="product_ID1" onblur="show_total()">
-                        <option value="placeholder" hidden>Please Select</option>
-                        <option value="1">Cheese</option>
-                        <option value="11">Milk</option>
-                        <option value="21">Cereal</option>
-                        <option value="31">Ice Cream</option>
-                    </select>
+        <div class="addorder" id="addorder">
+            <h1 class="white-text">Add New Order</h1>
+            <form method="post" action="processORDER.php" id="form" id="apply" novalidate="novalidate">
+                <div id="row_1">
+                    <div class="inputField">
+                        <label for="product_ID1">Product Name</label>
+                        <select name="product_ID1" id="product_ID1" onblur="show_total()">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="1">Cheese</option>
+                            <option value="11">Milk</option>
+                            <option value="21">Cereal</option>
+                            <option value="31">Ice Cream</option>
+                        </select>
+                    </div>
+                    <div class="inputField">
+                        <label for="quantity1">Quantity </label>
+                        <input type="number" min="1" max="100" name="quantity1" id="quantity1" required="required"
+                            onblur="show_total()" />
+                    </div>
+                    <div class="inputField">
+                        <!-- <label for="price1">Price </label> -->
+                        <select hidden name="price1" id="price1" onblur="show_total();">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="2">$2.0</option>
+                            <option value="4">$4.0</option>
+                            <option value="6">$6.0</option>
+                            <option value="8">$8.0</option>
+                        </select>
+                        <br>
+                    </div>
+                    <div class="inputField">
+                        <label>Amount </label>
+                        <label id="amount1">$0.00</label>
+                        <br>
+                    </div>
+                </div>
+                <div id="row_2">
+                    <div class="inputField">
+                        <label for="product_ID2">Product Name</label>
+                        <select name="product_ID2" id="product_ID2" onblur="show_total()">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="1">Cheese</option>
+                            <option value="11">Milk</option>
+                            <option value="21">Cereal</option>
+                            <option value="31">Ice Cream</option>
+                        </select>
+                    </div>
+                    <div class="inputField">
+                        <label for="quantity2">Quantity </label>
+                        <input type="number" min="1" max="100" name="quantity2" id="quantity2" onblur="show_total()" />
+                    </div>
+                    <div class="inputField">
+                        <!-- <label for="price2">Price </label> -->
+                        <select hidden name="price2" id="price2" onblur="show_total()">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="2">$2.0</option>
+                            <option value="4">$4.0</option>
+                            <option value="6">$6.0</option>
+                            <option value="8">$8.0</option>
+                        </select>
+                        <br>
+                    </div>
+                    <div class="inputField">
+                        <label>Amount </label>
+                        <label id="amount2">$0.00</label>
+                        <br>
+                    </div>
+                </div>
+                <div id="row_33">
+                    <div class="inputField">
+                        <label for="product_ID3">Product Name</label>
+                        <select name="product_ID3" id="product_ID3" onblur="show_total()">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="1">Cheese</option>
+                            <option value="11">Milk</option>
+                            <option value="21">Cereal</option>
+                            <option value="31">Ice Cream</option>
+                        </select>
+                    </div>
+                    <div class="inputField">
+                        <label for="quantity3">Quantity </label>
+                        <input type="number" min="1" max="100" name="quantity3" id="quantity3" onblur="show_total()" />
+                    </div>
+                    <div class="inputField">
+                        <!-- <label for="price3">Price </label> -->
+                        <select hidden name="price3" id="price3" onblur="show_total()">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="2">$2.0</option>
+                            <option value="4">$4.0</option>
+                            <option value="6">$6.0</option>
+                            <option value="8">$8.0</option>
+                        </select>
+                        <br>
+                    </div>
+                    <div class="inputField">
+                        <label>Amount </label>
+                        <label id="amount3">$0.00</label>
+                        <br>
+                    </div>
+                </div>
+                <div id="row_4">
+                    <div class="inputField">
+                        <label for="product_ID4">Product Name</label>
+                        <select name="product_ID4" id="product_ID4" onblur="show_total()">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="1">Cheese</option>
+                            <option value="11">Milk</option>
+                            <option value="21">Cereal</option>
+                            <option value="31">Ice Cream</option>
+                        </select>
+                    </div>
+                    <div class="inputField">
+                        <label for="quantity4">Quantity </label>
+                        <input type="number" min="1" max="100" name="quantity4" id="quantity4" onblur="show_total()" />
+                    </div>
+                    <div class="inputField">
+                        <!-- <label for="price4">Price </label> -->
+                        <select hidden name="price4" id="price4" onblur="show_total()">
+                            <option value="placeholder" hidden>Please Select</option>
+                            <option value="2">$2.0</option>
+                            <option value="4">$4.0</option>
+                            <option value="6">$6.0</option>
+                            <option value="8">$8.0</option>
+                        </select>
+                        <br>
+                    </div>
+                    <div class="inputField">
+                        <label>Amount </label>
+                        <label id="amount4">$0.00</label>
+                        <br>
+                    </div>
+                </div>
+                <div id="row_5">
+                    <div class="inputField">
+                        <label id="total_price"></label>
+                        <br>
+                        <label for="member_id">Member ID</label>
+                        <input type="text" name="member_id" minlength="10" placeholder=" Enter member's email"
+                            id="member_id" required="required" />
+                        <span id=member_error></span>
+                        <br>
+                    </div>
                 </div>
                 <div class="inputField">
-                    <label for="quantity1">Quantity </label>
-                    <input type="number" min="1" max="100" name="quantity1" id="quantity1" required="required"
-                        onblur="show_total()" />
-                </div>
-                <div class="inputField">
-                    <!-- <label for="price1">Price </label> -->
-                    <select hidden name="price1" id="price1" onblur="show_total();">
-                        <option value="placeholder" hidden>Please Select</option>
-                        <option value="2">$2.0</option>
-                        <option value="4">$4.0</option>
-                        <option value="6">$6.0</option>
-                        <option value="8">$8.0</option>
-                    </select>
+                    <button class="button" id="addordersubmit" buttonType="primary" type="submit" name="submit">Add
+                        Order</button>
                     <br>
                 </div>
-                <div class="inputField">
-                    <label>Amount </label>
-                    <label id="amount1">$0.00</label>
-                    <br>
+            </form>
+            <!-- Success dialog -->
+            <dialog class="success" id="success">
+                <div class="popup-status">
+                    <?php echo file_get_contents("./assets/icons/FaCheckCircle.svg"); ?>
+                    <p>Submission successful</p>
                 </div>
-            </div>
-            <div id="row_2">
-                <div class="inputField">
-                    <label for="product_ID2">Product Name</label>
-                    <select name="product_ID2" id="product_ID2" onblur="show_total()">
-                        <option value="placeholder" hidden>Please Select</option>
-                        <option value="1">Cheese</option>
-                        <option value="11">Milk</option>
-                        <option value="21">Cereal</option>
-                        <option value="31">Ice Cream</option>
-                    </select>
-                </div>
-                <div class="inputField">
-                    <label for="quantity2">Quantity </label>
-                    <input type="number" min="1" max="100" name="quantity2" id="quantity2" onblur="show_total()" />
-                </div>
-                <div class="inputField">
-                    <!-- <label for="price2">Price </label> -->
-                    <select hidden name="price2" id="price2" onblur="show_total()">
-                        <option value="placeholder" hidden>Please Select</option>
-                        <option value="2">$2.0</option>
-                        <option value="4">$4.0</option>
-                        <option value="6">$6.0</option>
-                        <option value="8">$8.0</option>
-                    </select>
-                    <br>
-                </div>
-                <div class="inputField">
-                    <label>Amount </label>
-                    <label id="amount2">$0.00</label>
-                    <br>
-                </div>
-            </div>
-            <div id="row_3">
-                <div class="inputField">
-                    <label id="total_price"></label>
-                    <br>
-                    <label for="member_id">Member ID</label>
-                    <input type="text" name="member_id" minlength="10" placeholder=" Enter member's email"
-                        id="member_id" required="required" />
-                    <span id=member_error></span>
-                    <br>
-                </div>
-            </div>
-            <div class="inputField">
-                <button class="button" id="addordersubmit" buttonType="primary" type="submit" name="submit">Add
-                    Order</button>
-                <br>
-            </div>
-        </form>
-        <!-- Success dialog -->
-        <dialog class="success" id="success">
-            <div class="popup-status">
-                <?php echo file_get_contents("./assets/icons/FaCheckCircle.svg"); ?>
-                <p>Submission successful</p>
-            </div>
-            <button class="button" onclick="location.href = 'addOrder.php';" buttonType="primary" type="submit"
-                name="submit">Add another order</button>
-        </dialog>
+                <button class="button" onclick="location.href = 'addOrder.php';" buttonType="primary" type="submit"
+                    name="submit">Add another order</button>
+            </dialog>
 
-        <!-- Error dialog -->
-        <dialog class="error" id="error">
-            <div class="popup-status">
-                <?php echo file_get_contents("./assets/icons/FaTimesCircle.svg"); ?>
-                <p>Oops! Something broke from our end. Please contact our technicians for support (Error code: 500)</p>
-            </div>
-            <button class="button" onclick="location.href = 'addOrder.php';" buttonType="primary" type="submit"
-                name="submit">Back to Add Order</button>
-        </dialog>
+            <!-- Error dialog -->
+            <dialog class="error" id="error">
+                <div class="popup-status">
+                    <?php echo file_get_contents("./assets/icons/FaTimesCircle.svg"); ?>
+                    <p>Oops! Something broke from our end. Please contact our technicians for support (Error code: 500)
+                    </p>
+                </div>
+                <button class="button" onclick="location.href = 'addOrder.php';" buttonType="primary" type="submit"
+                    name="submit">Back to Add Order</button>
+            </dialog>
     </main>
 </body>
 <?php
--- scripts/addorder.js
+++ scripts/addorder.js
@@ -6,7 +6,17 @@ function show_total() {
   var r2_p = document.getElementById("price2");
   var r2_qty = document.getElementById("quantity2").value.trim();
   var r2 = document.getElementById("row_2");
-  r2.style.display = "none";
+  var product_ID3 = document.getElementById("product_ID3").value;
+  var r3_p = document.getElementById("price3");
+  var r3_qty = document.getElementById("quantity3").value.trim();
+  var r3 = document.getElementById("row_33");
+  var product_ID4 = document.getElementById("product_ID4").value;
+  var r4_p = document.getElementById("price4");
+  var r4_qty = document.getElementById("quantity4").value.trim();
+  var r4 = document.getElementById("row_4");
+  // r2.style.display = "none";
+  // r3.style.display = "none";
+  // r4.style.display = "none";
   document.getElementById("total_price").innerText = "Price : $0";
   if (product_ID1 != "placeholder" && !r1_qty == "") {
     r2.style.display = "flex";
@@ -34,6 +44,7 @@ function show_total() {
     product_ID2 != "placeholder" &&
     !r2_qty == ""
   ) {
+    r3.style.display = "flex";
     if (product_ID2 == "1") {
       var r2_price = "2";
     }
@@ -54,14 +65,103 @@ function show_total() {
     num = num.toFixed(2);
     document.getElementById("total_price").innerText = "Price : $" + num;
   }
+
+  if (
+    product_ID1 != "placeholder" &&
+    !r1_qty == "" &&
+    product_ID2 != "placeholder" &&
+    !r2_qty == "" &&
+    product_ID3 != "placeholder" &&
+    !r3_qty == ""
+  ) {
+    r4.style.display = "flex";
+    if (product_ID3 == "1") {
+      var r3_price = "2";
+    }
+    if (product_ID3 == "11") {
+      var r3_price = "4";
+    }
+    if (product_ID3 == "21") {
+      var r3_price = "6";
+    }
+    if (product_ID3 == "31") {
+      var r3_price = "8";
+    }
+    r3_p.value = r3_price;
+    let num2 = r3_price * r3_qty;
+    num2 = num2.toFixed(2);
+    document.getElementById("amount3").innerText = "$" + num2;
+    let num = r1_price * r1_qty + r2_price * r2_qty + r3_price * r3_qty;
+    num = num.toFixed(2);
+    document.getElementById("total_price").innerText = "Price : $" + num;
+  }
+
+  if (
+    product_ID1 != "placeholder" &&
+    !r1_qty == "" &&
+    product_ID2 != "placeholder" &&
+    !r2_qty == "" &&
+    product_ID3 != "placeholder" &&
+    !r3_qty == "" &&
+    product_ID4 != "placeholder" &&
+    !r4_qty == ""
+  ) {
+    if (product_ID4 == "1") {
+      var r4_price = "2";
+    }
+    if (product_ID4 == "11") {
+      var r4_price = "4";
+    }
+    if (product_ID4 == "21") {
+      var r4_price = "6";
+    }
+    if (product_ID4 == "31") {
+      var r4_price = "8";
+    }
+    r4_p.value = r4_price;
+    let num3 = r4_price * r4_qty;
+    num3 = num3.toFixed(2);
+    document.getElementById("amount4").innerText = "$" + num3;
+    let num =
+      r1_price * r1_qty +
+      r2_price * r2_qty +
+      r3_price * r3_qty +
+      r4_price * r4_qty;
+    num = num.toFixed(2);
+    document.getElementById("total_price").innerText = "Price : $" + num;
+  }
 }
 function validate() {
   var product_ID1 = document.getElementById("product_ID1").value;
   var r1_qty = document.getElementById("quantity1").value.trim();
   var product_ID2 = document.getElementById("product_ID2").value;
   var r2_qty = document.getElementById("quantity2").value.trim();
+  var product_ID3 = document.getElementById("product_ID3").value;
+  var r3_qty = document.getElementById("quantity3").value.trim();
+  var product_ID4 = document.getElementById("product_ID4").value;
+  var r4_qty = document.getElementById("quantity4").value.trim();
   var member_id = document.getElementById("member_id").value.trim();
   var result = true;
+  if (
+    (product_ID4 == "placeholder" && r4_qty != "") ||
+    (product_ID4 != "placeholder" && r4_qty == "")
+  ) {
+    document.getElementById("row_4").classList.add("invalid");
+    result = false;
+  } else {
+    document.getElementById("row_4").classList.remove("invalid");
+    document.getElementById("row_4").classList.add("valid");
+  }
+  if (
+    (product_ID3 == "placeholder" && r3_qty != "") ||
+    (product_ID3 != "placeholder" && r3_qty == "")
+  ) {
+    document.getElementById("row_33").classList.add("invalid");
+    result = false;
+  } else {
+    document.getElementById("row_33").classList.remove("invalid");
+    document.getElementById("row_33").classList.add("valid");
+  }
   if (
     (product_ID2 == "placeholder" && r2_qty != "") ||
     (product_ID2 != "placeholder" && r2_qty == "")
@@ -81,12 +181,12 @@ function validate() {
   }
   const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!email_pattern.test(member_id) || member_id == "") {
-    document.getElementById("row_3").classList.add("invalid");
+    document.getElementById("row_5").classList.add("invalid");
     document.getElementById("member_error").innerText = "Invalid Member ID";
     result = false;
   } else {
-    document.getElementById("row_3").classList.remove("invalid");
-    document.getElementById("row_3").classList.add("valid");
+    document.getElementById("row_5").classList.remove("invalid");
+    document.getElementById("row_5").classList.add("valid");
     document.getElementById("member_error").innerText = "Valid Member ID";
   }
   //alert(result);
